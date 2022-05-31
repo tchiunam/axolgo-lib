bd13650e0af1543aae17abfda1566ced9d19eebb
tchiunam <tchiunam@users.noreply.github.com>
2022-05-31 23:37:27 +0800

    Add Parameter type for general purpose

---
+++ types/types.go
@@ -0,0 +1,10 @@
+package types
+
+// A data type for general purpose of parameters.
+type Parameter struct {
+	// The name of the parameter.
+	Name *string
+
+	// The value of the parameter.
+	Value *string
+}
