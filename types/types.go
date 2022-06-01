1eadf1c9b91ae82d3b0ca633b111c36ecde262ec
tchiunam <tchiunam@users.noreply.github.com>
2022-06-01 23:06:04 +0800

    Add a type of string flags

--- types/types.go
+++ types/types.go
@@ -1,10 +1,26 @@
 package types
 
-// A data type for general purpose of parameters.
+import "fmt"
+
+// A data type for general purpose of parameters
 type Parameter struct {
-	// The name of the parameter.
+	// The name of the parameter
 	Name *string
 
-	// The value of the parameter.
+	// The value of the parameter
 	Value *string
+}
+
+// A flag that accepts multiple strings as string array
+type StringArrayFlag []string
+
+// Output the value of flag
+func (s *StringArrayFlag) String() string {
+	return fmt.Sprint(*s)
+}
+
+// Append the value to array if the flag is used multiple times
+func (s *StringArrayFlag) Set(value string) error {
+	*s = append(*s, value)
+	return nil
 }
