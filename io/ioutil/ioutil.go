5f67145dcf097e728eba4755f21e1dcf36dc4d48
tchiunam <tchiunam@users.noreply.github.com>
2022-06-05 21:49:50 +0800

    Add function for reading yaml file

--- io/ioutil/ioutil.go
+++ io/ioutil/ioutil.go
@@ -1,10 +1,29 @@
 package ioutil
 
 import (
+	"github.com/spf13/viper"
 	"gopkg.in/ini.v1"
 )
 
 // Read the file given by filepath as an INI file
 func ReadIniFile(filepath string) (*ini.File, error) {
 	return ini.Load(filepath)
+}
+
+// Read the file given by filepath as a YAML file. Viper instance
+// is returned if optClass is not provided. Otherwise Unmarshalling
+// will be performed and the object will be returned.
+func ReadYamlFile(filepath string, optClass ...interface{}) (interface{}, error) {
+	v := viper.New()
+	v.SetConfigFile(filepath)
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	if optClass == nil {
+		return v, nil
+	} else {
+		err = v.Unmarshal(&optClass)
+		return optClass, err
+	}
 }
