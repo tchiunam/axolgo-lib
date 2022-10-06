9a42263560f579d2445a605d2bf41b11b41ee06d
tchiunam <tchiunam@users.noreply.github.com>
2022-10-06 21:06:30 +0800

    Validate address for blockchain

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -24,10 +24,13 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/gob"
 	"encoding/hex"
 	"fmt"
+	"log"
 
 	"github.com/tchiunam/axolgo-lib/util"
 )
@@ -41,15 +44,73 @@ type Transaction struct {
 
 // A transaction input
 type TXInput struct {
-	ID  []byte
-	Out int
-	Sig string
+	ID        []byte
+	Out       int
+	Signature []byte
+	PubKey    []byte
 }
 
 // A transaction output
 type TXOutput struct {
-	Value  int
-	PubKey string
+	Value      int
+	PubKeyHash []byte
+}
+
+// NewTxOutput creates a new transaction output
+func NewTxOutput(value int, address string) *TXOutput {
+	txo := &TXOutput{value, nil}
+	txo.Lock([]byte(address))
+
+	return txo
+}
+
+// UsesKey checks if the given address is used in the transaction
+func (in *TXInput) UsesKey(pubKeyHash []byte) bool {
+	lockingHash := PublicKeyHash(in.PubKey)
+
+	return bytes.Compare(lockingHash, pubKeyHash) == 0
+}
+
+// Lock locks the transaction output with the given address
+func (out *TXOutput) Lock(address []byte) error {
+	pubKeyHash, err := util.Base58Decode(address)
+	if err != nil {
+		return err
+	}
+
+	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
+	out.PubKeyHash = pubKeyHash
+
+	return nil
+}
+
+// IsLockedWithKey checks if the transaction output is locked with the
+// given public key hash
+func (out *TXOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+	return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
+
+// Serialize converts the transaction to a byte array
+func (tx Transaction) Serialize() []byte {
+	var encoded bytes.Buffer
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	util.PanicOnError(err)
+
+	return encoded.Bytes()
+}
+
+// Hash hashes the transaction
+func (tx *Transaction) Hash() []byte {
+	var hash [32]byte
+
+	txCopy := *tx
+	txCopy.ID = []byte{}
+
+	hash = sha256.Sum256(txCopy.Serialize())
+
+	return hash[:]
 }
 
 // Set the ID of the transaction
@@ -116,12 +177,31 @@ func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
 
-// Check if the transaction input can be unlocked with the given data
-func (in *TXInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
+func (tx *Transaction) Sign(privKey ecdsa.PrivateKey, prevTXs map[string]Transaction) {
+	if tx.IsCoinbase() {
+		return
+	}
 
-// Check if the transaction output can be unlocked with the given data
-func (out *TXOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
+	for _, in := range tx.Inputs {
+		if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("Previous transaction is not correct")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+
+	for inID, in := range txCopy.Inputs {
+		prevTx := prevTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inID].Signature = nil
+		txCopy.Inputs[inID].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inID].PubKey = nil
+
+		r, s, err := ecdsa.Sign(rand.Reader, &privKey, txCopy.ID)
+		util.PanicOnError(err)
+
+		signature := append(r.Bytes(), s.Bytes()...)
+
+		tx.Inputs[inID].Signature = signature
+	}
 }
